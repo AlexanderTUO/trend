9dfe0769462f068728c81e7c2af934c31509850a
AlexanderTUO <1599961496@qq.com>
2019-10-11 23:56:00 +0800

    'eureka-server'

---
+++ src/main/java/com/how2j/trend/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.how2j.trend;
+
+/**
+ * Hello world!
+ *
+ */
+public class EurekaServerApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!!!" );
+    }
+}
