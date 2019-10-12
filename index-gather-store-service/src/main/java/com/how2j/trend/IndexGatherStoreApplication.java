15d0eb598da40050734955998de2ed608f0d0219
tuoyangkun <1599961496@qq.com>
2019-10-12 15:14:48 +0800

    '采集和储存微服务-redis'

--- index-gather-store-service/src/main/java/com/how2j/trend/IndexGatherStoreApplication.java
+++ index-gather-store-service/src/main/java/com/how2j/trend/IndexGatherStoreApplication.java
@@ -6,6 +6,7 @@ import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
@@ -19,15 +20,22 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableCaching
 public class IndexGatherStoreApplication {
     public static void main(String[] args){
 
         int eurekaPort = 8761;
         int defaultPort = 8001;
         int port = defaultPort;
+        int redisPort = 6379;
 
         if (NetUtil.isUsableLocalPort(eurekaPort)) {
             System.err.printf("端口%d未启用，判断eureka服务未启动，无法启动本地服务%n",eurekaPort);
+            System.exit(1);
+        }
+
+        if (NetUtil.isUsableLocalPort(redisPort)) {
+            System.err.printf("端口%d未启用，判断redis服务未启动，本服务无法使用，故退出%n", redisPort);
             System.exit(1);
         }
 
@@ -46,6 +54,7 @@ public class IndexGatherStoreApplication {
             System.err.printf("端口%d被占用，无法启动%n", port);
             System.exit(1);
         }
+
 
         new SpringApplicationBuilder(IndexGatherStoreApplication.class).properties("server.port=" + port).run(args);
     }
--- index-gather-store-service/src/main/java/com/how2j/trend/service/IndexService.java
+++ index-gather-store-service/src/main/java/com/how2j/trend/service/IndexService.java
@@ -1,8 +1,11 @@
 package com.how2j.trend.service;
 
+import cn.hutool.core.collection.CollectionUtil;
 import com.how2j.trend.pojo.Index;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,11 +19,13 @@ import java.util.Map;
  * @Description:
  */
 @Service
+@CacheConfig(cacheNames = "indexes")
 public class IndexService {
     @Autowired
     private RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "thirdPartNotConnect")
+    @Cacheable(key = "'all_codes'")
     public List<Index> fetchDataFromThirdPart() {
         List<Map> temp = restTemplate.getForObject("http://127.0.0.1:8090/indexes/codes.json", List.class);
         return map2index(temp);
@@ -39,13 +44,11 @@ public class IndexService {
         return indexes;
     }
 
-    private List<Index> thirdPartNotConnect() {
+    public List<Index> thirdPartNotConnect() {
         System.out.println("第三方服务未连接");
         Index index = new Index();
         index.setCode("000000");
         index.setName("无效的指数代码");
-        List<Index> indices = new ArrayList<>();
-        indices.add(index);
-        return indices;
+        return CollectionUtil.toList(index);
     }
 }
