d0890a9f73dda83514d37eaaf931ddca63abfe87
AlexanderTUO <1599961496@qq.com>
2019-10-15 21:42:27 +0800

    模拟回测-指数代码

--- index-codes-service/src/main/java/com/how2j/trend/web/IndexController.java
+++ index-codes-service/src/main/java/com/how2j/trend/web/IndexController.java
@@ -4,6 +4,7 @@ import com.how2j.trend.config.Ipconfiguration;
 import com.how2j.trend.pojo.Index;
 import com.how2j.trend.service.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class IndexController {
     private Ipconfiguration ipconfiguration;
 
     @GetMapping("/codes")
+    @CrossOrigin
     private List<Index> get() {
         System.out.println("Current instance port is :"+ipconfiguration.getPort());
         return indexService.get();
